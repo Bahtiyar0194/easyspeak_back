abb0a1dc470210a195888c61269191721f3ff3fa
Baktiyar <bahtiyar_0194@mail.ru>
2026-09-15 15:08:19 +0500

    one hundred seventy six commit

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -37,5 +37,10 @@ return [
     'conference_has_not_started_yet' => 'Ожидается начало конференции...',
     'date_should_be_no_earlier_or_no_later_than_two_days' => 'Дата должна быть не раньше или не позже двух дней.',
     'choose_a_voice' => 'Выберите голос для генерации аудиофайла.',
-    'wrong_google_code' => 'Недействительный или истекший код от Google. Повторите попытку входа еще раз.'
+    'wrong_google_code' => 'Недействительный или истекший код от Google. Повторите попытку входа еще раз.',
+    'telegram' => [
+        'description' => 'Нажмите кнопку ниже для входа через Telegram',
+        'button'      => 'Войти через Telegram',
+        'script_error' => 'Telegram API еще не загружен. Повторите попытку позже.'
+    ]
 ];
